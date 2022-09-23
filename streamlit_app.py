b453f47af47c9983e8e62bafd872ec249dc918bd
reach2yuga1 <100332937+reach2yuga1@users.noreply.github.com>
2022-09-23 12:02:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,12 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 
-
-# write your own comment -what does the next line do? 
+import requests
+fruityvice_response = requests.get(https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
